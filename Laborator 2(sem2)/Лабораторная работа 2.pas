a9afc24f6f8e267d3f3483c749518a1a8acf7b65
DrigaNicolai <90197647+DrigaNicolai@users.noreply.github.com>
2022-03-30 16:18:01 +0300

    Lab2 and Lab3

---
+++ Laborator 2(sem2)/Лабораторная работа 2.pas
@@ -0,0 +1,49 @@
+﻿Program lab2;
+Type
+  Element = ^Nod;
+  Nod = Record
+    val: integer;
+    link: Element;
+  end;
+Var
+  stiva1, stiva2: Element;
+  temp: Element;
+  parc1, parc2: Element;
+  num: integer;
+  check: boolean;
+Begin
+  stiva1:= Nil;
+  stiva2:= Nil;
+  writeln('Introduceti numere pentru lista nr.1: ');
+  readln(num);
+  while (num <> 0) do
+   begin
+     New(temp);
+     temp^.val:= num;
+     temp^.link:= stiva1;
+     stiva1:= temp;
+     readln(num);
+    end;
+  writeln;
+  writeln('Introduceti numere pentru lista nr.2: ');
+  readln(num);
+  while (num <> 0) do
+   begin
+     New(temp);
+     temp^.val:= num;
+     temp^.link:= stiva2;
+     stiva2:= temp;
+     readln(num);
+    end;
+  writeln;
+  parc1:= stiva1;
+  parc2:= stiva2;
+  check:= true;
+  while((parc1 <> Nil) and (parc2 <> Nil) and check) do begin
+    if (parc1^.val <> parc2^.val) or ((parc1^.link = Nil) and (parc2^.link <> Nil)) or ((parc1^.link <> Nil) and (parc2^.link = Nil)) then check:= false;
+    parc1:= parc1^.link;
+    parc2:= parc2^.link;
+  end;
+  if (check) then writeln('Sunt identice')
+  else writeln('Nu sunt identice');
+End.
---
+++ Laborator 3(sem2)/Лабораторная работа 3.pas
@@ -0,0 +1,54 @@
+﻿Program lab3;
+type 
+  Element = ^Nod;
+  Nod = record
+    info : char;
+    link : Element;
+  end;
+var 
+  stiva : Element;
+  p : string;
+
+procedure pune(c:char);
+var r: Element;
+begin
+  new(r); 
+  r^.info:=c; 
+  r^.link:=stiva; 
+  stiva:=r;
+end;
+
+procedure scoate;
+var r: Element;
+begin
+  r:=stiva; 
+  stiva:=stiva^.link; 
+  dispose(r);
+end;
+
+function corect(t:string):boolean;
+var   i: integer;   G: 0..1;
+begin
+  G:=0;
+  i:=1;
+ while (i<=length(t)) and (G=0) do
+   begin
+     case t[i] of
+       '(','[','{': pune(t[i]);
+       ')': if (stiva <> Nil) and (stiva^.info='(') then scoate else G:=1;
+       ']': if (stiva <> Nil) and (stiva^.info='[') then scoate else G:=1;
+       '}': if (stiva <> Nil) and (stiva^.info='{') then scoate else G:=1;
+       else G:=1;
+     end;
+     i:=i+1;
+   end;
+    corect:= (stiva = Nil) and (G = 0);
+end;
+
+begin
+  write('Scrieti sirul de paranteze: '); 
+  readln(p);
+  stiva:=nil;
+  if corect(p) then writeln('Sir corect')
+  else writeln('Sir incorect');
+end.
