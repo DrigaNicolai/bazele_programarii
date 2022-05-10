87611f3af2efe73293ca7951857538f9fad03a85
DrigaNicolai <90197647+DrigaNicolai@users.noreply.github.com>
2022-05-10 11:12:05 +0300

    Add files via upload

--- Laborator 1(sem2)/Лабораторная работа 1.pas
+++ Laborator 1(sem2)/Лабораторная работа 1.pas
@@ -1,10 +1,13 @@
 ﻿Program test;
 Function count(n,x:integer):integer;
 begin
-  if(n mod 10 = x) then
-   count:=1+ count(n div 10, x)
-  else
-   count:=0 + count(n div 10, x);
+  if (n <> 0) then begin
+    if(n mod 10 = x) then
+     count:=1+ count(n div 10, x)
+    else
+     count:=0 + count(n div 10, x);
+  end
+  else count:= 0;
 end;
 var
   n, x: integer;
